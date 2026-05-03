78a0a7d5664a661b7e79240d44362174234bd20b
jaxbg <jayce.c.bg@gmail.com>
2026-05-03 14:29:38 +0400

    tips

--- src/pages/Garden.jsx
+++ src/pages/Garden.jsx
@@ -17,6 +17,17 @@ const tips = [
   "Avoid single-use plastics in line with your region's regulations.",
   "Compost leftover food scraps to create mulch.",
   "Using cold water for your laundry can save tons of energy.",
+  "Together, we can make a difference.",
+  "Every small action counts towards a greener future.",
+  "Global change starts with individual change.",
+  "We are responsible for tomorrow.",
+  "Help contribute to the UAE's Green Agenda - 2030.",
+  "Using apps like this supports UAE initiatives like 'We the UAE Vision 2031'.",
+  "Check the Oasis to find places ro properly dispose your waste.",
+  "Set your goals and see your progress in Plant.",
+  "You can use your seeds in Harvest to get rewards.",
+  "Head to Glean to check the carbon footprint of your food.",
+  "Keep track of your habits in Log."
 ];
 
 export default function Home() {
